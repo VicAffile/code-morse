98ca16ed047f5176a5e0ab9705a5787326759964
VicAffile <victoraffile@gmail.com>
2021-05-10 14:13:58 +0200

    classe led et bouton

--- code-morse.ino
+++ code-morse.ino
@@ -1,9 +1,38 @@
+char lettres[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
+class Bouton {
+  bool appuye;
+  uint8_t pin;
+  
+  public:
+  Bouton() : pin(PIN_INVALIDE), appuye(false) {}
+  Bouton(uint8_t pin) : pin(pin) {
+    pinMode(pin,INPUT_PULLUP);
+    appuye = digitalRead(pin);
+  }
+}
+
+class Led {
+  bool allumee;
+  uint8_t pin;
+
+  
+  public :
+  Led() : pin(PIN_INVALIDE), allumee(false) {}
+  Led(uint8_t pin) : pin(pin), allumee(false){
+    pinMode(pin,OUTPUT);
+  }
+}
+
 void setup()
 {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Port série ouvert ----------");
+
+  Bouton bouton = Bouton(12);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  
 }
